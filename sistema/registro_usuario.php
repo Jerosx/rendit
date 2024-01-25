9e3814d6097183d4c5692d95b4e6b033988f354c
Jerosx <jerobuitrago13@gmail.com>
2024-01-25 07:23:52 -0500

    Creación php envío de información desde formulario de variedades

---
+++ sistema/registro_usuario.php
@@ -0,0 +1,10 @@
+<?php
+$Nombres=$_POST['Nombres'];
+$Apellidos=$_POST['Apellidos'];
+$Docid=$_POST['Docid'];
+$Telefono=$_POST['Telefono'];
+$Correo=$_POST['Correo'];
+$Cargo=$_POST['Cargo'];
+$Codigo=$_POST['Codigo'];
+echo"making transition";
+?>
