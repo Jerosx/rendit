23fc39cf8c3c34f7fe7fcbdc546dba4d8ac11bf8
julian410 <julianocampo41@gmail.com>
2024-01-27 09:37:04 -0500

    Creacion php registro comercializadora

--- sistema/registro_comercializadora.php
+++ sistema/registro_comercializadora.php
@@ -1,4 +1,24 @@
 <?php
+include('conexion.php'); //llamo a la conexión a la base de datos
+
+$codigo=$_POST['codigo'];   #se crea variable comercializadora para tomar el dato con id=codigo en 'formcomercializadora.html'
+$nombre=$_POST['nombre'];   #se crea variable nombre para tomar el dato con id=nombre en 'formcomercializadora.html'
+
+#---------INICIO ENVIO INFO A LA BD, TBLcomercializadora------------------------
+$inscomercializadora=$con->query("insert into tblcomercializadora (codigo,nombre)
+                        values('$Codigo','$Nombre')");
+
+if($inscomercializadora){
+    echo"<h1> comercializadora registrada con exito. </h1>";
+}
+else{
+    echo"<h1> Error en el registro. </h1>";
+}
+#------------FIN ENVIO INFO A LA BD, TBLCOMERCIALIZADORA------------------------
+
+
+
+print "<a href='../formcomercializadora.html'> REGRESAR </a>"; #creo un botón para regresar al formulario
 
 
 ?>
