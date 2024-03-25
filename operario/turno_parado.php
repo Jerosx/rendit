e8976896aac7b3012d97e7c0c641c80221b53380
GaleXXX1 <alexandersotog.12@gmail.com>
2024-03-25 18:52:53 -0500

    Formulario de paro

--- operario/turno_parado.php
+++ operario/turno_parado.php
@@ -16,66 +16,72 @@ include ('../sistema/fecha.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../diseño/style.css">
     <title>Página de turno parado</title>
 
     <!-- <script src="../sistema/hora.js"></script> INHABILITO LA HORA POR FALLO--> 
 
 </head>
-<body>
-    
-    <p> <?php echo "Bienvenid@ <br> $nombreUsuario" ?> </p> <!--SALUDO Y NOMBRE -->
-        <p> <span id="hora"></span></p> <!-- HORA -->
-        <p> <?php echo "$fecha_actual" ?></p><!-- FECHA -->
+<body class="body-admin">
+    <header class="header-index-admin"> 
+        <div class="user-name">
+            <p> <?php echo "Bienvenid@ <br> $nombreUsuario" ?> </p> <!--SALUDO Y NOMBRE -->
+        </div>
         
-            <!-- Llama a la función actualizarHora al cargar la página -->
-                <script>
-                    window.onload = function() {
-                        actualizarHora();
-                    };
-                </script>
-            <!-- Llama a la función actualizarHora al cargar la página -->
-
-
-            <H1>TURNO PARADO</H1>
-
+        <div class="time-config">
+            <p> <span id="hora"></span></p> <!-- HORA -->
+            <p> <?php echo "$fecha_actual" ?></p><!-- FECHA -->
             
+                <!-- Llama a la función actualizarHora al cargar la página -->
+                    <script>
+                        window.onload = function() {
+                            actualizarHora();
+                        };
+                    </script>
+        </div>
+                <!-- Llama a la función actualizarHora al cargar la página -->
+    </header> 
+    <div class="main-admin">
+                <H1>TURNO PARADO</H1>
 
-        <h1>Cronómetro</h1>
-        <p id="timer">00:00</p>
-        <script src="../sistema/cronometro_pare.js"></script>
+                
+
+            <h1>Cronómetro</h1>
+            <p id="timer">00:00</p>
+            <script src="../sistema/cronometro_pare.js"></script>
 
 
-    <!--INICIO PESTAÑA MODAL RE-INICIAR TURNO -->
+        <!--INICIO PESTAÑA MODAL RE-INICIAR TURNO -->
 
-        <button id="btn-modal-retomar">RETOMAR TURNO</button>
+            <button class="admin-but" id="btn-modal-retomar">RETOMAR TURNO</button>
 
-        <dialog id="modal-retomar">
+            <dialog id="modal-retomar">
 
-            <h2> ATENCIÓN </h2>
-            <p> ¿Estás seguro de que quieres retomar tú turno?<p>
+                <h2> ATENCIÓN </h2>
+                <p> ¿Estás seguro de que quieres retomar tú turno?<p>
 
-            <form method="post" action="../sistema/retomar_turno.php">
+                <form method="post" action="../sistema/retomar_turno.php">
 
-                    <select name="opcion" id="opcion">
-                        <option disabled selected="">SELECCIONE "SI" PARA RETOMAR:</option>
-                        <option value="opcionSi">SI</option>
-                    </select>
-                    <button type="submit" value="Enviar">CONFIRMAR</button>
-            </form>
+                        <select name="opcion" id="opcion">
+                            <option disabled selected="">SELECCIONE "SI" PARA RETOMAR:</option>
+                            <option value="opcionSi">SI</option>
+                        </select>
+                        <button type="submit" value="Enviar">CONFIRMAR</button>
+                </form>
 
-            <button id="btn-cerrar-modal-retomar">AÚN NO VOY A RETOMAR</button>
+                <button class="admin-but" id="btn-cerrar-modal-retomar">AÚN NO VOY A RETOMAR</button>
 
-        </dialog>
+            </dialog>
 
-    <!--FIN PESTAÑA MODAL RE-INICIAR TURNO -->
+        <!--FIN PESTAÑA MODAL RE-INICIAR TURNO -->
 
-    <!--EL SCRIPT DEL MODAL VA AL FINAL PARA GARANTIZAR QUE SE CARGUE DESPUÉS DE LOS BOTONES QUE VA A USAR -->   
+        <!--EL SCRIPT DEL MODAL VA AL FINAL PARA GARANTIZAR QUE SE CARGUE DESPUÉS DE LOS BOTONES QUE VA A USAR -->   
 
-        <script src="../sistema/modal_retomar_turno.js"></script>         <!--Script controla modal inicio turno -->
+            <script src="../sistema/modal_retomar_turno.js"></script>         <!--Script controla modal inicio turno -->
 
-    <!--EL SCRIPT DEL MODAL VA AL FINAL PARA GARANTIZAR QUE SE CARGUE DESPUÉS DE LOS BOTONES QUE VA A USAR -->
-            
-
+        <!--EL SCRIPT DEL MODAL VA AL FINAL PARA GARANTIZAR QUE SE CARGUE DESPUÉS DE LOS BOTONES QUE VA A USAR -->
+                
+    </div>
 
 </body>
 </html>
