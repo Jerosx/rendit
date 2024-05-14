a05fe91f82ed8045b853839a5a0e079dbabea267
Cristian-corderovivero <cristiancordero1007@gmail.com>
2024-05-14 18:05:18 -0500

    MEJORA CREACIÓN DE NUEVOS USUARIOS

--- admin/formusuario.php
+++ admin/formusuario.php
@@ -12,44 +12,55 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Enlace al archivo CSS -->
     <link rel="stylesheet" href="../diseño/style.css">
+    <!-- Título de la página -->
     <title>Registro de nuevos usuarios</title>
+    <!-- Enlaces a Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
 </head>
 <body class="login">
     
-        <form class="myForm" action="../sistema/registro_usuario.php" method="post">
-            <div class="titulo">
-                <h1>Diligencie la información correspondiente</h1>
-            </div>
-            <div class="mb-3">
-                 <label class="form-label">NOMBRES</label><br/>
-                 <input type="text" class="form-control" name="Nombres">
-            </div>
-            <div class="mb-3">
-                <label class="form-label">APELLIDOS</label><br/>
-                <input type="text" class="form-control" name="Apellidos">
-           </div>
-            <div class="mb-3">
-                <label class="form-label">CÓDIGO PERSONAL</label><br/>
-                <input type="text" class="form-control" name="codigo">
-            </div>
-            <div class="mb-3">
-                <label class="form-label">CONTRASEÑA</label><br/>
-                <input type="text" class="form-control" name="contraseña">
-            </div>
-            <div class="mb-3">
-                <label class="form-label">CARGO</label><br/>
-                <input type="text" class="form-control" name="rol">
-            </div>
-            <div class="mb-3">
-                <label class="form-label">ESTADO</label><br/>
-                <input type="number" class="form-control" name="estado">
-            </div>
-            <button class="login-but" type="submit" class="bt-send">LISTO</button>
-        </form>
+    <!-- Formulario de registro de usuarios -->
+    <form class="myForm" action="../sistema/registro_usuario.php" method="post">
+        <!-- Título del formulario -->
+        <div class="titulo">
+            <h1>Diligencie la información correspondiente</h1>
+        </div>
+        <!-- Campo para ingresar nombres -->
+        <div class="mb-3">
+             <label class="form-label">NOMBRES</label><br/>
+             <input type="text" class="form-control" name="Nombres">
+        </div>
+        <!-- Campo para ingresar apellidos -->
+        <div class="mb-3">
+            <label class="form-label">APELLIDOS</label><br/>
+            <input type="text" class="form-control" name="Apellidos">
+       </div>
+        <!-- Campo para ingresar código personal -->
+        <div class="mb-3">
+            <label class="form-label">CÓDIGO PERSONAL</label><br/>
+            <input type="text" class="form-control" name="codigo">
+        </div>
+        <!-- Campo para ingresar contraseña -->
+        <div class="mb-3">
+            <label class="form-label">CONTRASEÑA</label><br/>
+            <input type="text" class="form-control" name="contraseña">
+        </div>
+        <!-- Lista desplegable para seleccionar el cargo -->
+        <div class="mb-3">
+            <label class="form-label">CARGO</label><br/>
+            <select name="rol" class="form-control">
+                <option value="Administrador">Administrador</option>
+                <option value="Operario">Operario</option>
+            </select>
+        </div>
+
+        <!-- Botón para enviar el formulario -->
+        <button class="login-but" type="submit" class="bt-send">LISTO</button>
+    </form>
     
 </body>
 </html>
--- sistema/registro_usuario.php
+++ sistema/registro_usuario.php
@@ -15,8 +15,50 @@ $Apellidos=$_POST['Apellidos'];
 $Codigo=$_POST['codigo'];
 $Rol=$_POST['rol'];
 $Contraseña=$_POST['contraseña'];
-$Estado = $_POST['estado'];
 
+
+#-------------inicio envio de la base de datos-----------#
+     switch($Rol){
+
+          case "Administrador";
+
+               echo ("Eligio Administrador");
+               
+               $insuser=$con->query("insert into tblusuario(Codigo,Contraseña,Nombre,Rol,Apellido, Estado) 
+                         values('$Codigo','$Contraseña','$Nombres', 1 ,'$Apellidos',1 )");
+
+                    if($insuser){
+                         echo"<h1> Usuario registrado con exito. </h1>";
+                    }
+                    else{
+                    echo"<h1> Error en el registro. </h1>";
+                    }
+                    break;
+
+               break;
+     
+          case  "Operario";
+
+              echo ("Eligio operario");
+           
+              $insuser=$con->query("insert into tblusuario(Codigo,Contraseña,Nombre,Rol,Apellido, Estado) 
+              values('$Codigo','$Contraseña','$Nombres', 1 ,'$Apellidos',1 )");
+
+                    if($insuser){
+                         echo"<h1> Usuario registrado con exito. </h1>";
+                    }
+                    else{
+                    echo"<h1> Error en el registro. </h1>";
+                    }
+                    break;
+
+               break; 
+     }
+
+     print "<a href='../admin/formusuario.php'> REGRESAR </a>"; #creo un botón para regresar al formulario
+
+#-------------fin envio de la base de datos-----------#
+/*
 #---------INICIO ENVIO INFO A LA BD, TBLUSUARIO------------------------
      $insuser=$con->query("insert into tblusuario(Codigo,Contraseña,Nombre,Rol,Apellido, Estado) 
                          values('$Codigo','$Contraseña','$Nombres','$Rol','$Apellidos','$Estado')");
@@ -32,6 +74,6 @@ $Estado = $_POST['estado'];
 
 print "<a href='../admin/formusuario.php'> REGRESAR </a>"; #creo un botón para regresar al formulario
 
-
+*/
 
 ?>
