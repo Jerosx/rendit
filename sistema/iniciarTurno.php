bda1946d156f808d09233fb41b9bfbc8700dab67
Jerosx <jerobuitrago13@gamil.com>
2024-05-20 12:22:15 -0500

    INCORPORACIÓN VALIDACION INICIO Y FIN TURNO (El programa ahora valida si la persona ya genero datos en HoraInicio y HoraFin, invalidando la posibilidad de que estén actualizando estos datos durante el día)

--- sistema/iniciarTurno.php
+++ sistema/iniciarTurno.php
@@ -1,55 +1,78 @@
 <?php
 #-----------------------------------CONEXIÓN A LA BD----------------------------------------
-    include ('conexion.php'); #me conecto a la BD
+include ('conexion.php'); #me conecto a la BD
 
-    # Verifico la conexión
-    if ($con->connect_error) {
-        die("Conexión fallida: " . $con->connect_error);
-
-    }
+# Verifico la conexión
+if ($con->connect_error) {
+    die("Conexión fallida: " . $con->connect_error);
+}
 #-----------------------------------CONEXIÓN A LA BD----------------------------------------
 
 #-----------------------------------INCLUDE----------------------------------------
+
+    include('fecha.php');
     include('validar_sesion.php'); #Incluyo este archivo para hacer uso de la variable "VALSESION", está almacena una consulta que contiene el codigo de quién inicia sesión en la app
+
 #-----------------------------------INCLUDE----------------------------------------
+
+#-----------------------------------CONSULTA EXISTENCIA DE TURNO----------------------------------------
+
+    $turno = "SELECT * FROM tblturno WHERE DATE(Fecha) = '$fecha_actual' AND Empacador = '$valsesion'";
+    $resultadoTurno = $con->query($turno);
+
+    echo "Op: " . $valsesion . "<br>"; #mensajes para depuración
+    echo "fecha: " . $fecha_actual . "<br>"; #mensajes para depuración
+    echo "Número de filas: " . $resultadoTurno->num_rows . "<br>"; #mensajes para depuración
+
+#-----------------------------------CONSULTA EXISTENCIA DE TURNO----------------------------------------
 
 #-------------------------------------INICIO ENVIO DE NOW A LA BD----------------------------------------------------------
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $opcionSeleccionada = $_POST["opcion"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $opcionSeleccionada = $_POST["opcion"];
 
-        if ($opcionSeleccionada == "opcionSi") { #Si elije SI, ejecuto el NOW a la BD para almacenar 'HoraInicio' en 'tblturno'
+    if ($opcionSeleccionada == "opcionSi") { #Si elije SI, ejecuto el NOW a la BD para almacenar 'HoraInicio' en 'tblturno'
         
+        if ($resultadoTurno === false) {
+            
+            echo "Error en la consulta: " . $con->error;
 
+        } elseif ($resultadoTurno->num_rows < 1) { #Si la variable resultadoTurno tiene un valor menor a 1, significa que no hay un turno registrado en la fecha de hoy 
+            
             $hora = "INSERT INTO tblturno(Codigo, Empacador, Fecha, HoraInicio, HoraFin, Cajas)
-                    VALUES(NULL, '$valsesion', NOW(), CURTIME(), '', '')"; #Hago el INSERT a la BD: 1)PK, 2)COD empacador de la sesion, 3)FECHA, 4)HoraInicio, 5)HoraFin, 6)Cajas
+                     VALUES(NULL, '$valsesion', NOW(), CURTIME(), '', '')"; #Hago el INSERT a la BD: 1)PK, 2)COD empacador de la sesion, 3)FECHA, 4)HoraInicio, 5)HoraFin, 6)Cajas
 
-        
             if ($con->query($hora) === TRUE) { #si todo sale bien
-
+                
                 echo "<script> alert('TURNO INICIADO EXITOSAMENTE');
                         window.location.href='../operario/indexope.php';
-                    </script>";
+                      </script>";
                 exit;
 
             } else { #si algo falla
-
+                
                 echo "<script> alert('FALLO AL INICIAR TURNO');
                         window.location.href='../operario/indexope.php';
-                    </script>"; $con->error;
+                      </script>";
+                echo $con->error;
 
             }
-
-        } else {#Si elije NO
-
-        
-            header("Location: ../operario/indexope.php"); #Lo devuelvo a la página principal 
-            exit; #Detengo la ejecución después de redirigirlo
+        } else { #Si la variable resultadoTurno tiene un valor mayor o igual a 1, significa que ya hay un turno registrado en la fecha de hoy 
+            
+            echo "<script> alert('USTED YA TIENE UN TURNO REGISTRADO EL DÍA DE HOY');
+                    window.location.href='../operario/indexope.php';
+                  </script>";
+            exit;
 
         }
+    } else { #Si elije NO
+        
+        header("Location: ../operario/indexope.php"); #Lo devuelvo a la página principal 
+        exit; #Detengo la ejecución después de redirigirlo
+
     }
-    $con->close(); #Cerrar conexión
+}
+$con->close(); #Cerrar conexión
+
 #-------------------------------------FIN ENVIO DE NOW A LA BD----------------------------------------------------------
-
-
 ?>
--- sistema/terminarTurno.php
+++ sistema/terminarTurno.php
@@ -1,56 +1,78 @@
 <?php
 #-----------------------------------CONEXIÓN A LA BD----------------------------------------
-    include ('conexion.php'); #me conecto a la BD
+    include('conexion.php'); #me conecto a la BD
 
     # Verifico la conexión
     if ($con->connect_error) {
         die("Conexión fallida: " . $con->connect_error);
-
     }
 #-----------------------------------CONEXIÓN A LA BD----------------------------------------
 
 #-----------------------------------INCLUDES----------------------------------------
-    include('validar_sesion.php'); #Incluyo este archivo para hacer uso de la variable "VALSESION", está almacena una consulta que contiene el codigo de quién inicia sesión en la app
+    include('validar_sesion.php'); #Incluyo este archivo para hacer uso de la variable "VALSESION", que almacena una consulta que contiene el código de quién inicia sesión en la app
+    include('fecha.php'); #Incluyo este archivo para hacer uso de la variable 'fecha_actual', que almacena el día en el que se está al abrir el aplicativo
+    #-----------------------------------INCLUDES----------------------------------------
+    include('fecha.php');
+#-----------------------------------CONSULTA EXISTENCIA DE TURNO YA FINALIZADO----------------------------------------
+    $turno = "SELECT HoraFin FROM tblturno WHERE DATE(Fecha) = '$fecha_actual' AND Empacador = '$valsesion'";
+    $resultadoFinTurno = $con->query($turno);
 
-    include('fecha.php'); #Incluyo este archivo para hacer uso de la variable 'fecha_actual´, está almacena el día en el que se está al abrir el aplicativo
+    if ($resultadoFinTurno === false) {
+        die("Error en la consulta: " . $con->error);
+    }
 
-#-----------------------------------INCLUDES----------------------------------------
+    echo "Op: " . $valsesion . "<br>"; #mensajes para depuración
+    echo "Fecha: " . $fecha_actual . "<br>"; #mensajes para depuración
+    echo "Turnos encontrados en el día: " . $resultadoFinTurno->num_rows . "<br>"; #mensajes para depuración
+
+    # Verificar el valor de HoraFin
+    $horaFin = null;
+    if ($resultadoFinTurno->num_rows > 0) {
+        $fila = $resultadoFinTurno->fetch_assoc();
+        $horaFin = $fila['HoraFin'];
+        echo "HoraFin: " . $horaFin . "<br>"; #mensajes para depuración
+    }
+#-----------------------------------CONSULTA EXISTENCIA DE TURNO YA FINALIZADO----------------------------------------
 
 #-------------------------------------INICIO ENVIO DE NOW A LA BD----------------------------------------------------------
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $opcionSeleccionada = $_POST["opcion"];
 
-        if ($opcionSeleccionada == "opcionSi") { #Si elije SI, ejecuto el NOW a la BD para almacenar 'HoraInicio' en 'tblturno'
-        
+        if ($opcionSeleccionada == "opcionSi") { # Si elige SI, ejecuto el NOW a la BD para almacenar 'HoraInicio' en 'tblturno'
+            
+            if ($horaFin == '00:00:00') { # Si HoraFin tiene valor 00:00:00, significa que su turno el día de hoy aún no se ha finalizado
+                $hora = "UPDATE tblturno SET HoraFin = NOW() 
+                        WHERE Empacador = '$valsesion' AND DATE(Fecha) = '$fecha_actual'"; # Actualizo el campo HoraFin filtrando por el operario con la sesión y por la fecha del día
 
-            $hora =("UPDATE tblturno SET HoraFin = NOW() 
-                                    WHERE Empacador = '$valsesion'
-                                    AND Fecha = '$fecha_actual'"); #Actualizo el campo HoraFin filtrando por el operario con la sesión y por la fecha del día
+                if ($con->query($hora) === TRUE) { # Si todo sale bien
 
-        
-            if ($con->query($hora) === TRUE) { #si todo sale bien
+                    echo "<script> alert('TURNO FINALIZADO EXITOSAMENTE');
+                            window.location.href='../operario/indexope.php';
+                        </script>";
+                    exit;
 
-                echo "<script> alert('TURNO FINALIZADO EXITOSAMENTE');
+                } else { # Si algo falla
+
+                    echo "<script> alert('FALLO AL FINALIZAR EL TURNO');
+                            window.location.href='../operario/indexope.php';
+                        </script>";
+                    echo "Error: " . $con->error;
+
+                }
+            } else { # Si HoraFin tiene un valor distinto de 00:00:00, significa que la persona ya finalizó el turno que tenía activo el día de hoy
+                
+                echo "<script> alert('USTED YA FINALIZÓ SU TURNO EL DÍA DE HOY');
                         window.location.href='../operario/indexope.php';
                     </script>";
-
-            } else { #si algo falla
-
-                echo "<script> alert('FALLO AL FINALIZAR EL TURNO');
-                        window.location.href='../operario/indexope.php';
-                    </script>"; $con->error;
+                exit;
 
             }
+        } else { # Si elige NO
 
-        } else {#Si elije NO
-
-        
-            header("Location: ../operario/indexope.php"); #Lo devuelvo a la página principal 
-            exit; #Detengo la ejecución después de redirigirlo
-
+            header("Location: ../operario/indexope.php"); # Lo devuelvo a la página principal
+            exit; # Detengo la ejecución después de redirigirlo
         }
     }
-    $con->close(); #Cerrar conexión
+    $con->close(); # Cerrar conexión
 #-------------------------------------FIN ENVIO DE NOW A LA BD----------------------------------------------------------
-print "<a href='../operario/indexope.php'> REGRESAR </a>"; #creo un botón para regresar al formulario
 ?>
