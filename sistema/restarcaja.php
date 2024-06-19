412a14601e5f12300534da1c811c3da9507eba4f
Jerosx <jerobuitrago13@gamil.com>
2024-06-19 07:33:43 -0500

    Arreglo en restarcaja.php, ahora no permite bajar de 0 en las cajas empacadas

--- sistema/restarcaja.php
+++ sistema/restarcaja.php
@@ -13,24 +13,63 @@
     include('fecha.php'); #Incluyo este archivo para hacer uso de la variable 'fecha_actual´, está almacena el día en el que se está al abrir el aplicativo
 #-----------------------------------INCLUDE----------------------------------------
 
+#-------------------------------------INICIO VALIDAR QUE CAJAS NO SEA - A 0----------------------------------------------------------
+
+    $cajas = "SELECT Cajas FROM tblturno WHERE (Fecha) = '$fecha_actual' AND Empacador = '$valsesion'"; #Genero una consulta para bsucar cuantas cajas tiene registrado el turno
+    $resultadoCajas = $con->query($cajas);
+        
+        if ($resultadoCajas) #Si se ejecuta la consulta
+        {
+            
+            if ($fila = $resultadoCajas->fetch_assoc()) #Si se encuentra al menos un resultado 
+            {
+
+                $cajasEncontradas = $fila['Cajas']; #almaceno la cantidad en la variable cajasEncontradas
+                echo $cajasEncontradas; #mensaje para depuracion
+
+            } 
+            else #manejo de posible error
+            {
+
+                echo "No se encontraron resultados.";
+
+            }
+        } 
+        else #manejo de error en consulta
+        {
+            
+            echo "Error en la consulta: " . $con->error;
+            
+        }
+
+#-------------------------------------FIN VALIDAR QUE CAJAS NO SEA - A 0----------------------------------------------------------
+
 #-------------------------------------INICIO RESTAR CAJAS A LA BD----------------------------------------------------------
 
-    $restarCaja = $con->query("UPDATE tblturno
-                                SET Cajas = Cajas - 1
-                                WHERE Empacador = '$valsesion'
-                                AND Fecha = '$fecha_actual'"); #Creo la consulta que le resta 1 a la cantidad de cajas que hay filtrando por empacador y fecha del día
-        
-        if($restarCaja === TRUE) #Valido que todo salga bien
-        {
-            echo "Caja restada"; #mensaje depuración
-            header ("location: ../operario/indexTurno.php"); #lo devuelvo al index directamente
-            exit(); #detengo la ejecución del script
-        }
-        else
-        {
-            echo "Error al restar la caja" . $con->error; #mensaje depuración
-            header ("location: ../operario/indexTurno.php"); #lo devuelvo al index directamente
-        }
+    if($cajasEncontradas>0)
+    {
+        $restarCaja = $con->query("UPDATE tblturno
+                                    SET Cajas = Cajas - 1
+                                    WHERE Empacador = '$valsesion'
+                                    AND Fecha = '$fecha_actual'"); #Creo la consulta que le resta 1 a la cantidad de cajas que hay filtrando por empacador y fecha del día
+            
+            if($restarCaja === TRUE) #Valido que todo salga bien
+            {
+                echo "Caja restada"; #mensaje depuración
+                header ("location: ../operario/indexTurno.php"); #lo devuelvo al index directamente
+                exit(); #detengo la ejecución del script
+            }
+            else
+            {
+                echo "Error al restar la caja" . $con->error; #mensaje depuración
+                header ("location: ../operario/indexTurno.php"); #lo devuelvo al index directamente
+            }
+    }
+    else
+    {
+        echo "No se puede tener valores negativos en este campo"; #depuracion 
+        header ("location: ../operario/indexTurno.php"); #lo devuelvo al index directamente
+    }
 
 #-------------------------------------FIN RESTAR CAJAS A LA BD----------------------------------------------------------
 $con->close(); #cierro la conexión a la bd
